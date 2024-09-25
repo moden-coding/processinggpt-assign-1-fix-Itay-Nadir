fec4cb4f3853e0e832ff4f9a99f86ad119b0c45f
Itay <28inadir@gannacademy.org>
2024-09-25 08:31:26 -0400

    Chat Gpt practice assigment done

--- src/App.java
+++ src/App.java
@@ -4,4 +4,26 @@ public class App extends PApplet {
     public static void main(String[] args)  {
         PApplet.main("App");
     }
+        public void settings() {
+            size(800, 600); 
+        }
+    
+        public void setup() {
+            background(0, 100, 0); 
+        }
+    
+        public void draw() {
+            background(0, 0, 255); 
+    
+            fill(0,255,0);
+            rect(100,100,200,100);
+
+            fill(255,0,0);
+            triangle(100,600,350,400,350,600);
+
+            stroke(0,10,0);
+            strokeWeight(5);
+            line(100,200,300,500);
+    }
+
 }
